6193deb1a31884f4932c7d4cca6511f227060054
Tyler Reymer <treymer0@gmail.com>
2026-03-14 17:30:12 -0700

    infra: add ACM SSL certificate terraform

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,13 @@ output "website_bucket_arn" {
   description = "S3 bucket ARN for website"
   value       = aws_s3_bucket.website.arn
 }
+
+output "acm_certificate_arn" {
+  description = "ACM certificate ARN"
+  value       = aws_acm_certificate.website.arn
+}
+
+output "acm_validation_records" {
+  description = "DNS validation records to add to Cloudflare"
+  value       = aws_acm_certificate.website.domain_validation_options
+}
